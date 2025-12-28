15c00c2c805d4fe3c031c759a4199b1fa50fd1c6
DESKTOP-FUQTUKJ\Administrator <759761629@qq.com>
2025-12-28 23:08:07 +0800

    feat: stock数据

--- lucky/a-stock/filter.mjs
+++ lucky/a-stock/filter.mjs
@@ -22,52 +22,56 @@ import { writeFileSync, readFileSync } from 'node:fs'
     "nmc": 480238.239928, // 流通市值
     "turnoverratio": 14.16045, // 换手率，计算公式为成交量/流通股本×100%，反映股票流通性强弱
     "date": "2025-10-31"
-*/ 
+*/
 const filterResult = () => {
-    const data = readFileSync(`data.json`, 'utf-8')
-    const stockMap = JSON.parse(data)
-    const list = []
-    for (let key in stockMap) {
-        const stock = stockMap[key]
-        const len = stock.length
-        if (len < 2) {
-            continue
-        }
-
-        // const stock1 = stock[1]
-        // const stock2 = stock[2]
-        // const stock3 = stock[3]
-        // const stock4 = stock[4]
-
-        const isUP = stock.every(item => item.changepercent > 0)
-
-        const pre = stock[len - 2]
-        const cur = stock[len - 1]
-
-        // 成交额2倍 成交量2倍 当日(最低价-开盘)/卡盘 < 0.1
-        if ((cur.amount / pre.amount) > 3 && 
-            (cur.volume / pre.volume) > 1 &&
-            pre.changepercent > 0 &&
-            cur.changepercent > 0 && 
-            cur.trade <= 20 && 
-            cur.trade > cur.open &&
-            // pre.trade > pre.open &&
-            // pre.trade > (cur.high + cur.low) / 2 && 
-            // cur.turnoverratio > 10 &&
-            isUP &&
-            cur.symbol.indexOf('bj') === -1) {
-            list.push({
-                name: key,
-                code: cur.code,
-                price: cur.trade,
-                changepercent: cur.changepercent,
-                date: cur.date
-            })
-            // console.log(list)
-        }
+  const data = readFileSync(`data.json`, 'utf-8')
+  const stockMap = JSON.parse(data)
+  const list = []
+  for (let key in stockMap) {
+    const stock = stockMap[key]
+    const len = stock.length
+    if (len < 2) {
+      continue
     }
-    console.log(list, list.length)
 
-    writeFileSync('filter.json', JSON.stringify(list, null, 2), 'utf-8')
+    // const stock1 = stock[1]
+    // const stock2 = stock[2]
+    // const stock3 = stock[3]
+    // const stock4 = stock[4]
+
+    const isUP = stock.every((item) => item.changepercent > 0)
+
+    const pre = stock[len - 2]
+    const cur = stock[len - 1]
+
+    // 成交额2倍 成交量2倍 当日(最低价-开盘)/卡盘 < 0.1
+    if (
+      cur.amount / pre.amount > 3 &&
+      cur.volume / pre.volume > 2 &&
+      //   pre.changepercent > 0 &&
+      cur.changepercent > 5 &&
+      cur.trade <= 20 &&
+      //   cur.trade > cur.open &&
+      // pre.trade > pre.open &&
+      // pre.trade > (cur.high + cur.low) / 2 &&
+      cur.turnoverratio > 8 &&
+      cur.turnoverratio < 20 &&
+      //   isUP &&
+      !cur.code.startsWith('300') &&
+      cur.symbol.indexOf('bj') === -1
+    ) {
+      list.push({
+        name: key,
+        code: cur.code,
+        price: cur.trade,
+        changepercent: cur.changepercent,
+        date: cur.date
+      })
+      // console.log(list)
+    }
+  }
+  console.log(list, list.length)
+
+  writeFileSync('filter.json', JSON.stringify(list, null, 2), 'utf-8')
 }
 filterResult()
